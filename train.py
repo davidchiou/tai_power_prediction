5242d12cf2d5fded7f0aec2132d88c89722e1bd5
davidchiou <davidchiou@outlook.com>
2017-10-05 15:50:46 +0800

    http://scikit-learn.org/stable/modules/generated/sklearn.metrics.explained_variance_score.html#sklearn.metrics.explained_variance_score regression 要用這個測accuracy

--- train.py
+++ train.py
@@ -1,5 +1,6 @@
 import pandas
 from sklearn import linear_model
+from sklearn.metrics import explained_variance_score
 
 train = pandas.read_csv("train.csv")
 train_data = []
@@ -13,6 +14,7 @@ for index, row in train[train['TownName']=="松山區"].iterrows():
 
 reg = linear_model.LinearRegression()
 reg.fit(train_data[:int(0.6*len(train_data))], train_label[:int(0.6*len(train_label))])
-print(reg.predict([train_data[32]]))
+predict_label = reg.predict(train_data[int(0.6*len(train_data)):])
 print(reg.coef_)
+print(explained_variance_score(train_label[int(0.6*len(train_label)):], predict_label))
 #test.to_csv('out.csv')
